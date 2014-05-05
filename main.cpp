f6e00bc35dde2b1ff81fb4047d68581377ad3ddb
lichgo <lichgo88@gmail.com>
2014-05-05 22:28:34 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HackingCPP
+//
+//  Created by lichgo on 5/5/14.
+//  Copyright (c) 2014 lichgo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

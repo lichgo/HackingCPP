8a98004743d5067189e09377b314bfba3414547c
lichgo <lichgo88@gmail.com>
2014-05-09 08:05:02 +0800

    pointer

--- using_array.cpp
+++ using_array.cpp
@@ -7,3 +7,29 @@
 //
 
 #include "using_array.h"
+
+using std::cout;
+
+int get_size() {
+    return 4;
+}
+
+int main()
+{
+    const unsigned buf_size = 52;
+    int staff_size = 12;
+    const int tree_size = get_size();
+    
+    char a1[buf_size];
+    char a2[buf_size + 1];
+    int a3[staff_size];
+    int a4[get_size()];
+    int a5[tree_size];
+    
+    cout << a1[0] << a3[7];
+    
+    for (int *p = a4; )
+    
+    
+    return 0;
+}
--- using_vector.cpp
+++ using_vector.cpp
@@ -8,7 +8,35 @@
 
 #include "using_vector.h"
 
-int mai()
+#include <vector>
+using std::vector;
+using std::string;
+
+int m()
 {
+    // initialization
+    vector<int> v1;
+    vector<int> v2(v1);
+    vector<int> v3(10, 'a');
+    vector<int> v4(100);    //100 0s
+    vector<string> v5(5);   //5 empty strings
+    
+    // iterator
+    for (vector<int>::iterator iter = v3.begin(); iter != v3.end(); ++iter)
+    {
+        *iter = 'b';
+    }
+    std::cout << (char)v3[5] << std::endl;
+    
+    // const_iterator
+    vector<int>::const_iterator c_iter = v3.begin();
+    //*c_iter = 'a';  //error, the pointed ele is read-only
+    c_iter++;       //ok
+    
+    // compared with const iterator
+    const vector<int>::iterator C_ITER = v3.begin();
+    *C_ITER = 'c';  //ok
+    //*C_ITER++;  // error
+    
     return 0;
 }

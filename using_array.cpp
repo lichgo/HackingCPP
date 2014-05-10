1e4281004a4d729a65fa738219c09a385df62058
lichgo <lichgo88@gmail.com>
2014-05-11 01:16:16 +0800

    using pointer

--- using_array.cpp
+++ using_array.cpp
@@ -12,28 +12,44 @@ using std::cout;
 using std::endl;
 
 int get_size() {
-    return 4;
+    int result = 5;
+    
+    std::cin >> result;
+    
+    return result;
 }
 
-int main()
+int main_array()
 {
+    // The length should be a constant that can be known in compile time
     const unsigned buf_size = 52;
-    int staff_size = 12;
+    int staff_size = 2;
     const int tree_size = get_size();
     
-    char a1[buf_size];
-    char a2[buf_size + 1];
-    int a3[staff_size];
-    int a4[get_size()];
-    int a5[tree_size];
+//    char a1[buf_size];
+//    char a2[buf_size + 1];
+//    
+//    int a3[staff_size]; //error
+//    a3[5] = 4;
+//    
+//    int a4[get_size()]; //normal
+//    a4[9] = 10;
+//    for (int *p_start = a4, *p_end = a4 + get_size(); p_start != p_end; p_start++)
+//    {
+//        cout << "Num" << *p_start << endl;
+//        *p_start = 324;
+//    }
     
-    cout << a1[0] << a3[7] << endl;;
-    
-    for (int *p_start = a4, *p_end = a4 + get_size(); p_start != p_end; p_start++)
-    {
-        cout << *p_start << endl;
-    }
-    
+    int a5[tree_size];  //error
+    a5[9] = 12;
+    cout << "a5 " << a5[9] << endl;
+
+//    cout << a1[0] << a3[7] << endl;;
+
+    // array of chars as string
+    char s1[] = { 'C' };
+    char s2[] = { 'C', '\0' };
+    char s3[] = "C";    // same as s2, with null at the end
     
     return 0;
 }

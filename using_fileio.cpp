e063824eca184e464d9ec803565a92a155a64b2a
lichgo <lichgo88@gmail.com>
2014-05-11 21:34:13 +0800

    collection

--- using_fileio.cpp
+++ using_fileio.cpp
@@ -9,20 +9,53 @@
 #include "using_fileio.h"
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
+int main_usingfileio()
 {
     string in_file_dir = "README.md";
     string out_file_dir = "cpy_README.md";
     
-//    ifstream infile(in_file_dir.c_str());
-//    ofstream outfile(out_file_dir.c_str());
+    ifstream infile(in_file_dir.c_str());
+    ofstream outfile(out_file_dir.c_str());
     
-    ifstream infile;
-    ofstream outfile;
+//    ifstream infile;
+//    ofstream outfile;
+//    infile.open(in_file_dir.c_str());
+//    outfile.open(out_file_dir.c_str());
     
-    infile.open(in_file_dir.c_str());
-    outfile.open(out_file_dir.c_str());
+    vector<string> files(3, "README.md");
+    ifstream in_file;
+    string s;
+    vector<string>::const_iterator beg = files.begin();
+    while (beg != files.end())
+    {
+        in_file.open(beg->c_str());
+        
+        if (!in_file)
+            break;
+        
+        while (in_file >> s)
+        {
+            cout << s << endl;
+        }
+        in_file.close();
+        in_file.clear();    //without this, stream cannot be reset
+        
+        beg++;
+    }
+    
+    return 0;
+}
+
+ifstream &open_file(ifstream &in, const string &file)
+{
+    in.close();
+    in.clear();
+    in.open(file.c_str());
+    return in;
 }
---
+++ using_stringstream.cpp
@@ -0,0 +1,46 @@
+//
+//  using_stringstream.cpp
+//  HackingCPP
+//
+//  Created by lichgo on 11/5/14.
+//  Copyright (c) 2014 lichgo. All rights reserved.
+//
+
+#include "using_stringstream.h"
+#include <sstream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istringstream;
+using std::ostringstream;
+
+int main()
+{
+    string line, word;
+    while (getline(cin, line))
+    {
+        if (line == "done") break;
+        
+        istringstream stream(line);     // bind line to a stringstream
+        
+        while (stream >> word)
+        {
+            cout << word << endl;
+        }
+    }
+    
+    int val1 = 12, val2 = 23;
+    ostringstream format;
+    format << "val1: " << val1 << "\tval2: " << val2;
+    
+    istringstream from_format(format.str());
+    int val3;   //0 by default
+    int val4;
+    string dump;
+    from_format >> dump >> val3 >> dump >> val4;
+    cout << val3 << " " << val4;
+    
+    return 0;
+}

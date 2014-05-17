c7ba5aa8af761a1007c0af106ea951baeb04591e
lichgo <lichgo88@gmail.com>
2014-05-17 10:11:36 +0800

    43

--- 04_02_trim_string.cpp
+++ 04_02_trim_string.cpp
@@ -6,11 +6,11 @@ using namespace std;
 void trim(string& s, char c) {
     // from beginning
     string::iterator iter = s.begin();
-//    while (iter != s.end() && *iter == c) iter++;
-//    s.erase(s.begin(), iter);
+    while (iter != s.end() && *iter == c) iter++;
+    s.erase(s.begin(), iter);
     
     // from end
-    iter = s.end();
+    iter = --s.end();
     while (iter != s.begin() && *iter == c) iter--;
     if (*iter != c) iter++;
     s.erase(iter, s.end());
@@ -21,29 +21,25 @@ int main_0402() {
     // first pointer == last pointer in erase, remove nothing
     string s = "hallo";
     s.erase(s.begin(), s.begin());
-    cout << s;
+    cout << s << "|" << endl;
     
     trim(s, ' ');
-    cout << s << endl;
+    cout << s << "|" << endl;
     
     s = "hallo ";
     trim(s, ' ');
-    cout << s << endl;
+    cout << s << "|" << endl;
     
     s = "hallo***";
     trim(s, ' ');
-    cout << s << endl;
+    cout << s << "|" << endl;
     
     trim(s, '*');
-    cout << s << endl;
+    cout << s << "|" << endl;
     
     s = "**  *hallo***";
     trim(s, '*');
-    cout << s << endl;
+    cout << s << "|" << endl;
     
     return 0;
-}
-
-int main() {
-    main_0402();
 }
---
+++ 04_03_storing_string_in_sequence.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+void write(const string& s) {
+    cout << s << '\n';
+}
+
+void main_0403() {
+    
+    // vector
+    vector<string> v;
+    
+    string s = "one";
+    v.push_back(s);
+    s = "two";
+    v.push_back(s);
+    s = "three";
+    v.push_back(s);
+    
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << '\n';
+    }
+    
+    
+    // list
+    list<string> l;
+    
+    l.push_front("knife");
+    l.push_front("spoon");
+    l.push_front("fork");
+    
+    for_each(l.begin(), l.end(), write);
+}
+
+int main() {
+    main_0403();
+}
